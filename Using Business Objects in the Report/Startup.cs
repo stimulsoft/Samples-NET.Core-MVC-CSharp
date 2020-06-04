3035fe82910e939b9c7d7e0d47d842c67883dc3e
Arash zandi <Arashz44@gmail.com>
2020-06-04 09:23:11 -0700

    Update Startup.cs

--- Using Business Objects in the Report/Startup.cs
+++ Using Business Objects in the Report/Startup.cs
@@ -28,16 +28,16 @@ namespace Using_Business_Objects_in_the_Report
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            // }
-            // else
-            //{
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            //}
+            }
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
